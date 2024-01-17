2f9f327bdca5257b7a5b6d014d857a0183f9d4fd
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-17 07:29:01 -0500

    HU 34 Se crea paquete Comida y se crea clase Desayuno

---
+++ src/main/java/org/example/comida/Desayuno.java
@@ -0,0 +1,8 @@
+package org.example.comida;
+
+public class Desayuno {
+    public static void main (String[]args){
+        System.out.println("Pan con huevo");
+        System.out.println("Huevo con yema blanda");
+    }
+}
